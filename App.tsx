7649f34da07300de27a110ec50f68375069a3c77
Can Kanbur <cankanbur1@gmail.com>
2022-11-18 21:14:24 +0300

    install and setup apollo client

--- App.tsx
+++ App.tsx
@@ -4,7 +4,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+const API_KEY =
+  'itaperucu::stepzen.net+1000::d4e42159c8880dd79fcdb6fb18b622b8efcd8f6dc01cf3614a817ea97c280fd3';
+const client = new ApolloClient({
+  uri: 'https://itaperucu.stepzen.net/api/billowing-alligator/__graphql',
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
@@ -13,10 +22,10 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
+      <ApolloProvider client={client}>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
-      </SafeAreaProvider>
+      </ApolloProvider>
     );
   }
 }
