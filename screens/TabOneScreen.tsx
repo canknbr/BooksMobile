b1ff355784438a46831fdcf46f096105ec2bcfb8
Can Kanbur <cankanbur1@gmail.com>
2022-11-19 13:32:22 +0300

    adds filtering search options

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -43,7 +43,26 @@ const query = gql`
 
 export default function TabOneScreen() {
   const [search, setSearch] = useState('');
+  const [provider, setProvider] = useState<
+    'googleBooksSearch' | 'openLibrarySearch'
+  >('googleBooksSearch');
   const [runQuery, { data, loading, error }] = useLazyQuery(query);
+  const parseBook = (item: any): Book => {
+    if (provider === 'googleBooksSearch') {
+      return {
+        title: item.volumeInfo?.title,
+        image: item.volumeInfo.imageLinks.thumbnail,
+        authors: item.volumeInfo.authors,
+        isbn: item.volumeInfo.industryIdentifiers?.[0]?.identifier,
+      };
+    }
+    return {
+      image: `https://covers.openlibrary.org/b/olid/${item.cover_edition_key}-M.jpg`,
+      title: item.title,
+      authors: item.author_name,
+      isbn: item.isbn?.[0],
+    };
+  };
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -58,6 +77,28 @@ export default function TabOneScreen() {
           onPress={() => runQuery({ variables: { q: search } })}
         />
       </View>
+      <View style={styles.tabs}>
+        <Text
+          style={
+            provider === 'googleBooksSearch'
+              ? { fontWeight: 'bold', color: 'royalblue' }
+              : {}
+          }
+          onPress={() => setProvider('googleBooksSearch')}
+        >
+          Google Books
+        </Text>
+        <Text
+          style={
+            provider === 'openLibrarySearch'
+              ? { fontWeight: 'bold', color: 'royalblue' }
+              : {}
+          }
+          onPress={() => setProvider('openLibrarySearch')}
+        >
+          Open Library
+        </Text>
+      </View>
       {loading && <ActivityIndicator />}
       {error && (
         <View style={styles.container}>
@@ -66,17 +107,12 @@ export default function TabOneScreen() {
         </View>
       )}
       <FlatList
-        data={data?.googleBooksSearch?.items || []}
-        renderItem={({ item }) => (
-          <BookItem
-            book={{
-              title: item.volumeInfo.title,
-              image: item.volumeInfo.imageLinks.thumbnail,
-              authors: item.volumeInfo.authors,
-              isbn: item.volumeInfo.industryIdentifiers?.[0]?.identifier,
-            }}
-          />
-        )}
+        data={
+          (provider === 'googleBooksSearch'
+            ? data?.googleBooksSearch?.items
+            : data?.openLibrarySearch?.docs) || []
+        }
+        renderItem={({ item }) => <BookItem book={parseBook(item)} />}
         showsVerticalScrollIndicator={false}
       />
     </View>
@@ -97,6 +133,13 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  tabs: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: 50,
+    alignItems: 'center',
+  },
+  tab: {},
   header: {
     flexDirection: 'row',
     alignItems: 'center',
